f8fad9763b41cc955b1c3cce002d244602d9158f
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-16 16:46:16 +0300

    feat(useToggle): Реализовать задачу.

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,3 +1,32 @@
+import { useLayoutEffect } from 'react'
+import { useReducer } from 'react'
+
+const reducer = (state, action) => {
+  const index = action.payload.indexOf(state)
+
+  switch (action.type) {
+    case 'TOGGLE':
+      return action.payload instanceof Array
+        ? action.payload[(index + 1) % action.payload.length]
+        : action.payload
+    default:
+      return state
+  }
+}
+
 export function useToggle(initialValue) {
+  const [value, dispatch] = useReducer(reducer, initialValue)
+
+  useLayoutEffect(() => {
+    toggle()
+  }, [])
+
+  const toggle = (value = initialValue) => {
+    dispatch({
+      type: 'TOGGLE',
+      payload: value,
+    })
+  }
+
   return [value, toggle]
 }
